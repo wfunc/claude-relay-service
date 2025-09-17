105972ed68adb3c25b3e09e756a50cb8b47e2c71
wfunc <114468522+wfunc@users.noreply.github.com>
2025-09-17 14:55:21 +0800

    feat:  modify check-updates use mine (CANNOT-PR)

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -5701,7 +5701,7 @@ router.get('/check-updates', authenticateAdmin, async (req, res) => {
     }
 
     // 请求 GitHub API
-    const githubRepo = 'wei-shaw/claude-relay-service'
+    const githubRepo = 'wfunc/claude-relay-service'
     const response = await axios.get(`https://api.github.com/repos/${githubRepo}/releases/latest`, {
       headers: {
         Accept: 'application/vnd.github.v3+json',
